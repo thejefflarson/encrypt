c2220705f9247c315bbef45faec0677b3c6215eb
Jeff Larson <thejefflarson@gmail.com>
2014-07-07 12:18:21 -0400

    draft decrypt routine

--- src/encrypt.c
+++ src/encrypt.c
@@ -7,10 +7,12 @@
 #include "randombytes.h"
 #include "tweetnacl.h"
 
+static const int padding = crypto_secretbox_NONCEBYTES + crypto_onetimeauth_BYTES;
+
 static int
-map_pair(char *ppath, char *spath,
-         unsigned char *text,
-         unsigned char *create,
+map_pair(const char *ppath, char const *spath,
+         unsigned char **text,
+         unsigned char **create,
          size_t *size,
          int tweak) {
   struct stat st;
@@ -22,14 +24,14 @@ map_pair(char *ppath, char *spath,
   *size = st.st_size;
 
   pid = open(ppath, O_RDONLY);
-  text = (unsigned char *) mmap(NULL, *size, PROT_READ, MAP_ANON | MAP_SHARED, pid, 0);
-  if(text == MAP_FAILED) err = 1;
+  *text = (unsigned char *) mmap(NULL, *size, PROT_READ, MAP_ANON | MAP_SHARED, pid, 0);
+  if(*text == MAP_FAILED) err = 1;
 
   csize = *size + tweak;
   sid = open(spath, O_WRONLY | O_CREAT);
   err = ftruncate(sid, csize);
-  create = (unsigned char *) mmap(NULL, csize, PROT_WRITE, MAP_ANON | MAP_SHARED, sid, 0);
-  if(creat == MAP_FAILED) err = 1;
+  *create = (unsigned char *) mmap(NULL, csize, PROT_WRITE, MAP_ANON | MAP_SHARED, sid, 0);
+  if(*create == MAP_FAILED) err = 1;
 
   close(sid);
   close(pid);
@@ -44,32 +46,50 @@ close_pair(unsigned char *text, unsigned char *create, size_t size, int tweak){
 }
 
 int
-encrypt(char *ppath, char *spath,
-        unsigned char pk[crypto_box_PUBLICKEYBYTES],
-        unsigned char sk[crypto_box_SECRETKEYBYTES]){
+encrypt(const char *ppath, const char *spath,
+        const unsigned char pk[crypto_box_PUBLICKEYBYTES],
+        const unsigned char sk[crypto_box_SECRETKEYBYTES]){
   unsigned char *secret = NULL;
   unsigned char *plain  = NULL;
   unsigned char n[crypto_box_NONCEBYTES];
   unsigned char k[crypto_box_BEFORENMBYTES];
-  int tweak = crypto_secretbox_NONCEBYTES + crypto_onetimeauth_BYTES;
   size_t size;
 
-  int err = map_pair(ppath, spath, plain, secret, &size, tweak);
+  int err = map_pair(ppath, spath, &plain, &secret, &size, padding);
   if(err != 0) return err;
 
   randombytes(n, crypto_box_NONCEBYTES);
   crypto_box_beforenm(k, pk, sk);
-  crypto_stream_xor(secret + tweak, plain, size, n, k);
+  crypto_stream_xor(secret + padding, plain, size, n, k);
   memcpy(secret, n, crypto_secretbox_NONCEBYTES);
-  crypto_onetimeauth(secret + crypto_secretbox_NONCEBYTES, secret + tweak, size, k);
+  crypto_onetimeauth(secret + crypto_secretbox_NONCEBYTES, secret + padding, size, k);
   memset(k, 0, crypto_box_BEFORENMBYTES);
-  close_pair(plain, secret, size, tweak);
+  close_pair(plain, secret, size, padding);
+
   return 0;
 }
 
 int
-decrypt(char *spath, char *ppath,
-        unsigned char pk[crypto_box_PUBLICKEYBYTES],
-        unsigned char sk[crypto_box_SECRETKEYBYTES]) {
-  return 0;
+decrypt(const char *spath, const char *ppath,
+        const unsigned char pk[crypto_box_PUBLICKEYBYTES],
+        const unsigned char sk[crypto_box_SECRETKEYBYTES]) {
+  unsigned char *secret = NULL;
+  unsigned char *plain  = NULL;
+  unsigned char k[crypto_box_BEFORENMBYTES];
+  size_t size;
+
+  int err = map_pair(ppath, spath, &secret, &plain, &size, -1 * padding);
+  if(err != 0) return err;
+
+  crypto_box_beforenm(k, pk, sk);
+  err = crypto_onetimeauth_verify(secret + crypto_secretbox_NONCEBYTES, secret + padding, size, k);
+  if(err == 0) {
+    unsigned char n[crypto_box_NONCEBYTES];
+    memcpy(n, secret, crypto_secretbox_NONCEBYTES);
+    crypto_stream_xor(plain, secret + padding, size - padding, n ,k);
+  }
+
+  memset(k, 0, crypto_box_BEFORENMBYTES);
+  close_pair(secret, plain, size, -1 * padding);
+  return err;
 }
