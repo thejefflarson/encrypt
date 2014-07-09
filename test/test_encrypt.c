e4d1f0980053bd9bdabab1f873ec5ff31ac7f6a5
Jeff Larson <thejefflarson@gmail.com>
2014-07-09 17:47:26 -0400

    waypoint

--- test/test_encrypt.c
+++ test/test_encrypt.c
@@ -1,7 +1,17 @@
 #include "tap.h"
+#include "encrypt.h"
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int
 main(){
+  int err = encrypt("encrypt.in", "encrypt.out");
+  ok(err == 0, "encrypted the file");
+  err = decrypt("encrypt.out", "decrypt.out");
+  ok(err == 0, "decrypted the file");
+  struct stat st;
+  // test that everything works out.
 
+  ok(ret, "files are identical");
   return 0;
 }
